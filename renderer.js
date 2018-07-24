6c163e3d24d38f2ba7b7de83ed1bdfa77671770c
dkipp <mail@daniel-kipp.de>
2018-07-24 14:07:12 +0200

    added navigation sections

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+// https://github.com/crilleengvall/electron-tutorial-app/blob/master/assets/js/menu.js
+
+function importSectionsToDOM() {
+  const links = document.querySelectorAll('link[rel="import"]')
+  Array.prototype.forEach.call(links, function (link) {
+    let template = link.import.querySelector('.section-template')
+    let clone = document.importNode(template.content, true)
+    document.querySelector('#content').appendChild(clone)
+  })
+}
+
+importSectionsToDOM();
